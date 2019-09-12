623de484d462a2f7b4bdba02b6e3dd2a9098e4c4
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-12 10:03:37 +0200

    Add project files.

---
+++ BeerControlCms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerControlCms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du leg J/N?");
+            var hasLeg = Console.ReadLine() == "J";
+
+            Console.WriteLine("Hur gammal är du?");
+            var ageInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Var är du (1=krogen, 2=systemet)");
+            var locationInput= Console.ReadLine();
+            var locationType = Rules.AlcoholeBuyingRules.LocationType.None;
+            if(locationInput == "1")
+                locationType = Rules.AlcoholeBuyingRules.LocationType.Krogen;
+            else if (locationInput == "2")
+                locationType = Rules.AlcoholeBuyingRules.LocationType.Systemet;
+
+            Console.WriteLine("Vad är din promillehalt");
+            var promilleHalt = Convert.ToDecimal(Console.ReadLine());
+
+            var canBuy = Rules.AlcoholeBuyingRules.CanBuyAlcohol(
+                ageInput, locationType, hasLeg, promilleHalt);
+            if (canBuy)
+                Console.WriteLine("Hurra");
+
+        }
+    }
+}
---
+++ BeerControlCms/Rules/AlcoholeBuyingRules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerControlCms.Rules
+{
+    class AlcoholeBuyingRules
+    {
+        public enum LocationType
+        {
+            None,
+            Krogen,
+            Systemet
+        }
+        static public bool CanBuyAlcohol(int age, LocationType location, bool hasLeg, decimal promille)
+        {
+            if (!hasLeg) return false;
+            if (promille >= 1.0m) return false;
+            if (age >= 18 && location == LocationType.Krogen) return true;
+            if (age >= 20 && location == LocationType.Systemet) return true;
+            return false;
+        }
+    }
+}
